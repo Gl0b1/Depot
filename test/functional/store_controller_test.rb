ca4865ed998bb017ef00bed802376b252804930f
Gl0b1 <smbulle@hotail.com>
2011-03-29 16:17:28 +0200

    kapitel 8

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -6,13 +6,16 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-# 
-  validates :title, :uniqueness => true
-  validates :image_url, :format => {
-    :with    => %r{\.(gif|jpg|png)$}i,
-    :message => 'must be a URL for GIF, JPG or PNG image.'
-  }
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_select '#columns #side a', :minimum => 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
 end
